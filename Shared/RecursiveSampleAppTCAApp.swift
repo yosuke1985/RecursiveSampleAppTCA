599086fa58b7a8d824a4312b8d305c1e5897c0e6
Yosuke NAKAYAMA <ihatenonoildressing@gmail.com>
2022-04-15 23:32:48 +0900

    Initial Commit

---
+++ Shared/RecursiveSampleAppTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecursiveSampleAppTCAApp.swift
+//  Shared
+//
+//  Created by Yosuke NAKAYAMA on 2022/04/15.
+//
+
+import SwiftUI
+
+@main
+struct RecursiveSampleAppTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
